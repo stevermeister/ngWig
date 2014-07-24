729e011db8141a5313d6e47d66a569bdea32e05e
Volodymyr Dziubak <volodymyr.dzyubak@gmail.com>
2014-07-23 22:10:31 -0500

    initial styling

--- src/javascript/app/templates.js
+++ src/javascript/app/templates.js
@@ -2,29 +2,59 @@ angular.module('ngwig-app-templates', ['ng-wig/views/ng-wig.html']);
 
 angular.module("ng-wig/views/ng-wig.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("ng-wig/views/ng-wig.html",
-    "<div class=\"ngwig\">\n" +
-    "  <div class=\"ngwig-header\">\n" +
-    "    <div class=\"ngwig-control bold\" title=\"Bold\" ng-click=\"execCommand('bold')\"></div>\n" +
-    "    <div class=\"ngwig-control italic\" title=\"Italic\" ng-click=\"execCommand('italic')\"></div>\n" +
-    "    <div class=\"ngwig-control underline\" title=\"Underline\" ng-click=\"execCommand('underline')\"></div>\n" +
-    "    <div class=\"ngwig-divider\"></div>\n" +
-    "    <div class=\"ngwig-control paragraph\" title=\"Paragraph\" ng-click=\"execCommand('formatblock', '<p>')\"></div>\n" +
-    "    <div class=\"ngwig-control h1\" title=\"H1\" ng-click=\"execCommand('formatblock', '<h1>')\"></div>\n" +
-    "    <div class=\"ngwig-control h2\" title=\"H2\" ng-click=\"execCommand('formatblock', '<h2>')\"></div>\n" +
-    "    <div class=\"ngwig-control h3\" title=\"H3\" ng-click=\"execCommand('formatblock', '<h3>')\"></div>\n" +
-    "    <div class=\"ngwig-divider\"></div>\n" +
-    "    <div class=\"ngwig-control orderedlist\" title=\"Insert Ordered List\" ng-click=\"execCommand('insertorderedlist')\"></div>\n" +
-    "    <div class=\"ngwig-control unorderedlist\" title=\"Insert Unordered List\" ng-click=\"execCommand('insertunorderedlist')\"></div>\n" +
-    "    <div class=\"ngwig-divider\"></div>\n" +
-    "    <div class=\"ngwig-control leftalign\" title=\"Left Align\" ng-click=\"execCommand('justifyleft')\"></div>\n" +
-    "    <div class=\"ngwig-control centeralign\" title=\"Center Align\" ng-click=\"execCommand('justifycenter')\"></div>\n" +
-    "    <div class=\"ngwig-control rightalign\" title=\"Right Align\" ng-click=\"execCommand('justifyright')\"></div>\n" +
-    "    <div class=\"ngwig-control blockalign\" title=\"Block Justify\" ng-click=\"execCommand('justifyfull')\"></div>\n" +
-    "    <div ng-show=\"debug\" class=\"ngwig-control blockalign\" title=\"Block Justify\" ng-click=\"editMode = !editMode\"></div>\n" +
+    "<div class=\"ng-wig\">\n" +
+    "  <div class=\"l-block\">\n" +
+    "    <div class=\"l-list l-list--inline\">\n" +
+    "      <div class=\"l-list__item\">\n" +
+    "        <div class=\"nw-toolbar\">\n" +
+    "          <ul class=\"l-list l-list--inline l-list--collapsed\">\n" +
+    "            <li class=\"l-list__item\">\n" +
+    "              <button class=\"nw-toolbar__action editor-icon-paragraph\" title=\"Paragraph\" ng-click=\"execCommand('formatblock', '<p>')\"></button>\n" +
+    "            </li><!--\n" +
+    "            --><li class=\"l-list__item\">\n" +
+    "              <button class=\"nw-toolbar__action nw-toolbar__action--last editor-icon-header\" title=\"Header\" ng-click=\"execCommand('formatblock', '<h1>')\"></button>\n" +
+    "            </li>\n" +
+    "          </ul>\n" +
+    "        </div>\n" +
+    "      </div>\n" +
+    "\n" +
+    "      <div class=\"l-list__item\">\n" +
+    "        <div class=\"nw-toolbar\">\n" +
+    "          <ul class=\"l-list l-list--inline l-list--collapsed\">\n" +
+    "            <li class=\"l-list__item\">\n" +
+    "              <button class=\"nw-toolbar__action editor-icon-bold\" title=\"Bold\" ng-click=\"execCommand('bold')\"></button>\n" +
+    "            </li><!--\n" +
+    "            --><li class=\"l-list__item\">\n" +
+    "              <button class=\"nw-toolbar__action nw-toolbar__action--last editor-icon-italic\" title=\"Italic\" ng-click=\"execCommand('italic')\"></button>\n" +
+    "            </li>\n" +
+    "          </ul>\n" +
+    "        </div>\n" +
+    "      </div>\n" +
+    "\n" +
+    "      <div class=\"l-list__item\">\n" +
+    "        <div class=\"nw-toolbar\">\n" +
+    "          <ul class=\"l-list l-list--inline l-list--collapsed\">\n" +
+    "            <li class=\"l-list__item\">\n" +
+    "              <button class=\"nw-toolbar__action editor-icon-ul\" title=\"Insert Unordered List\" ng-click=\"execCommand('insertunorderedlist')\"></button>\n" +
+    "            </li><!--\n" +
+    "            --><li class=\"l-list__item\">\n" +
+    "              <button class=\"nw-toolbar__action nw-toolbar__action--active editor-icon-ol\" title=\"Insert Ordered List\" ng-click=\"execCommand('insertorderedlist')\"></button>\n" +
+    "            </li><!--\n" +
+    "            --><li class=\"l-list__item\">\n" +
+    "              <button class=\"nw-toolbar__action editor-icon-link\"></button>\n" +
+    "            </li><!--\n" +
+    "            --><li class=\"l-list__item\">\n" +
+    "              <button class=\"nw-toolbar__action nw-toolbar__action--last editor-icon-unlink\"></button>\n" +
+    "            </li>\n" +
+    "          </ul>\n" +
+    "        </div>\n" +
+    "      </div>\n" +
+    "    </div>\n" +
     "  </div>\n" +
-    "  <div>\n" +
-    "    <textarea class=\"ngwig-editor\" ng-show=\"editMode\" ng-model=\"content\"></textarea>\n" +
-    "    <iframe class=\"ngwig-editor\" ng-hide=\"editMode\" ng-model=\"content\" ng-wig-editable></iframe>\n" +
+    "\n" +
+    "  <div class=\"nw-editor\">\n" +
+    "    <textarea class=\"nw-editor__src\" ng-show=\"editMode\" ng-model=\"content\"></textarea>\n" +
+    "    <iframe class=\"nw-editor__res\" frameBorder=\"0\" ng-hide=\"editMode\" ng-model=\"content\" ng-wig-editable></iframe>\n" +
     "  </div>\n" +
     "</div>");
 }]);
