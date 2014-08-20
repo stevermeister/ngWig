ac132389be642e4a5db96b1842fd6a3cec5ac825
Stepan Suvorov <stevermeister@gmail.com>
2014-08-20 10:08:32 +0200

    update version

--- dist/ng-wig.js
+++ dist/ng-wig.js
@@ -80,54 +80,29 @@ angular.module('ngwig-app-templates', ['ng-wig/views/ng-wig.html']);
 angular.module("ng-wig/views/ng-wig.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("ng-wig/views/ng-wig.html",
     "<div class=\"ng-wig\">\n" +
-    "  <div class=\"l-block nw-header\">\n" +
-    "    <div class=\"l-list l-list--inline\">\n" +
-    "      <div class=\"l-list__item\">\n" +
-    "        <div class=\"nw-toolbar\">\n" +
-    "          <ul class=\"l-list l-list--inline l-list--collapsed\">\n" +
-    "            <li class=\"l-list__item\">\n" +
-    "              <button type=\"button\" class=\"nw-toolbar__action editor-icon-paragraph\" title=\"Paragraph\" ng-click=\"execCommand('formatblock', '<p>')\"></button>\n" +
-    "            </li><!--\n" +
-    "            --><li class=\"l-list__item\">\n" +
-    "              <button type=\"button\" class=\"nw-toolbar__action nw-toolbar__action--last editor-icon-header\" title=\"Header\" ng-click=\"execCommand('formatblock', '<h1>')\"></button>\n" +
-    "            </li>\n" +
-    "          </ul>\n" +
-    "        </div>\n" +
-    "      </div>\n" +
-    "\n" +
-    "      <div class=\"l-list__item\">\n" +
-    "        <div class=\"nw-toolbar\">\n" +
-    "          <ul class=\"l-list l-list--inline l-list--collapsed\">\n" +
-    "            <li class=\"l-list__item\">\n" +
-    "              <button type=\"button\" class=\"nw-toolbar__action editor-icon-bold\" title=\"Bold\" ng-click=\"execCommand('bold')\"></button>\n" +
-    "            </li><!--\n" +
-    "            --><li class=\"l-list__item\">\n" +
-    "              <button type=\"button\" class=\"nw-toolbar__action nw-toolbar__action--last editor-icon-italic\" title=\"Italic\" ng-click=\"execCommand('italic')\"></button>\n" +
-    "            </li>\n" +
-    "          </ul>\n" +
-    "        </div>\n" +
-    "      </div>\n" +
-    "\n" +
-    "      <div class=\"l-list__item\">\n" +
-    "        <div class=\"nw-toolbar\">\n" +
-    "          <ul class=\"l-list l-list--inline l-list--collapsed\">\n" +
-    "            <li class=\"l-list__item\">\n" +
-    "              <button type=\"button\" class=\"nw-toolbar__action editor-icon-ul\" title=\"Insert Unordered List\" ng-click=\"execCommand('insertunorderedlist')\"></button>\n" +
-    "            </li><!--\n" +
-    "            --><li class=\"l-list__item\">\n" +
-    "              <button type=\"button\" class=\"nw-toolbar__action nw-toolbar__action--active editor-icon-ol\" title=\"Insert Ordered List\" ng-click=\"execCommand('insertorderedlist')\"></button>\n" +
-    "            </li><!--\n" +
-    "            --><li class=\"l-list__item\">\n" +
-    "              <button type=\"button\" class=\"nw-toolbar__action editor-icon-link\"></button>\n" +
-    "            </li><!--\n" +
-    "            --><li class=\"l-list__item\">\n" +
-    "              <button type=\"button\" class=\"nw-toolbar__action nw-toolbar__action--last editor-icon-unlink\"></button>\n" +
-    "            </li>\n" +
-    "          </ul>\n" +
-    "        </div>\n" +
-    "      </div>\n" +
-    "    </div>\n" +
-    "  </div>\n" +
+    "  <ul class=\"nw-toolbar\">\n" +
+    "    <li class=\"nw-toolbar__item\">\n" +
+    "      <button type=\"button\" class=\"nw-button nw-button--header-one\" title=\"Header\" ng-click=\"execCommand('formatblock', '<h1>')\"></button>\n" +
+    "    </li><!--\n" +
+    "    --><li class=\"nw-toolbar__item\">\n" +
+    "      <button type=\"button\" class=\"nw-button nw-button--paragraph\" title=\"Paragraph\" ng-click=\"execCommand('formatblock', '<p>')\"></button>\n" +
+    "    </li><!--\n" +
+    "    --><li class=\"nw-toolbar__item\">\n" +
+    "      <button type=\"button\" class=\"nw-button nw-button--unordered-list\" title=\"Unordered List\" ng-click=\"execCommand('insertunorderedlist')\"></button>\n" +
+    "    </li><!--\n" +
+    "    --><li class=\"nw-toolbar__item\">\n" +
+    "      <button type=\"button\" class=\"nw-button nw-button--ordered-list\" title=\"Ordered List\" ng-click=\"execCommand('insertorderedlist')\"></button>\n" +
+    "    </li><!--\n" +
+    "    --><li class=\"nw-toolbar__item\">\n" +
+    "      <button type=\"button\" class=\"nw-button nw-button--bold\" title=\"Bold\" ng-click=\"execCommand('bold')\"></button>\n" +
+    "    </li><!--\n" +
+    "    --><li class=\"nw-toolbar__item\">\n" +
+    "      <button type=\"button\" class=\"nw-button nw-button--italic\" title=\"Italic\" ng-click=\"execCommand('italic')\"></button>\n" +
+    "    </li><!--\n" +
+    "    --><li class=\"nw-toolbar__item\">\n" +
+    "      <button type=\"button\" class=\"nw-button nw-button--link\"></button>\n" +
+    "    </li>\n" +
+    "  </ul>\n" +
     "\n" +
     "  <div class=\"nw-editor\">\n" +
     "    <textarea class=\"nw-editor__src\" ng-show=\"editMode\" ng-model=\"content\"></textarea>\n" +
