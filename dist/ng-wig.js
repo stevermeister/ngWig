3521f0b3acc9ed1ca5f140d6a91a1f636ea1ae68
Shahrul Nizam <hottincup@gmail.com>
2014-12-12 22:21:26 +0800

    Add dynamic binding to html - require sanitize

--- dist/ng-wig.js
+++ dist/ng-wig.js
@@ -180,6 +180,23 @@ angular.module('ngWig').directive('ngWigEditable', function () {
     }
 );
 
+/**
+ * Allow dynamic binding to html
+ * require angular-sanitize module loaded
+ * to use;
+ * <ng-wig-html to-bind="text2"></ng-wig-html>
+ * 'text2' is the actual model assign in the ng-wig directive 
+**/
+angular.module('ngWig').directive('ngWigHtml', function ($compile) {
+	 return {
+        restrict: "AE",
+		link: function(scope, element, attr)  {
+			var ele = $compile('<div ng-bind-html="'+attr.toBind+'"></div>')(scope);
+			element.replaceWith(ele);
+		}
+    }
+});
+
 angular.module('ngwig-app-templates', ['ng-wig/views/ng-wig.html']);
 
 angular.module("ng-wig/views/ng-wig.html", []).run(["$templateCache", function($templateCache) {
