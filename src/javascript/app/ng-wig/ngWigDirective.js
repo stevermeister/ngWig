eed58b8a3d1b54b31ee8e82b4cca1aa92d8b0c9c
aris <aris@localhost>
2015-07-14 18:38:43 +0300

    Integrated nw-button--text-color from PR #54

--- src/javascript/app/ng-wig/ngWigDirective.js
+++ src/javascript/app/ng-wig/ngWigDirective.js
@@ -1,43 +1,41 @@
 angular.module('ngWig').directive('ngWig', function () {
 
-      return {
-        scope: {
-          content: '=ngWig'
-        },
-        restrict: 'A',
-        replace: true,
-        templateUrl: 'ng-wig/views/ng-wig.html',
-        link: function (scope, element, attrs) {
+  return {
+    scope: {
+      content: '=ngWig'
+    },
+    restrict: 'A',
+    replace: true,
+    templateUrl: 'ng-wig/views/ng-wig.html',
+    link: function (scope, element, attrs) {
+      scope.editMode = false;
+      scope.autoexpand = !('autoexpand' in attrs) || attrs['autoexpand'] !== 'off';
+      scope.cssPath = scope.cssPath ? scope.cssPath : 'css/ng-wig.css';
 
-          scope.originalHeight = element.outerHeight();
-          scope.editMode = false;
-          scope.autoexpand = !('autoexpand' in attrs) || attrs['autoexpand'] !== 'off';
-          scope.cssPath = scope.cssPath ? scope.cssPath : 'css/ng-wig.css';
+      scope.toggleEditMode = function () {
+        scope.editMode = !scope.editMode;
+      };
 
-          scope.toggleEditMode = function() {
-            scope.editMode = !scope.editMode;
-          };
+      scope.execCommand = function (command, options) {
+        if (command === 'createlink') {
+          options = prompt('Please enter the URL', 'http://');
+        }
+        scope.$emit('execCommand', {command: command, options: options});
+      };
 
-          scope.execCommand = function (command, options) {
-            if(command ==='createlink'){
-              options = prompt('Please enter the URL', 'http://');
-            }
-            scope.$emit('execCommand', {command: command, options: options});
-          };
+      scope.styles = [
+        {name: 'Normal text', value: 'p'},
+        {name: 'Header 1', value: 'h1'},
+        {name: 'Header 2', value: 'h2'},
+        {name: 'Header 3', value: 'h3'}
+      ];
 
-          scope.styles = [
-            {name: 'Normal text', value: 'p'},
-            {name: 'Header 1', value: 'h1'},
-            {name: 'Header 2', value: 'h2'},
-            {name: 'Header 3', value: 'h3'}
-          ];
+      scope.style = scope.styles[0];
 
-        scope.style = scope.styles[0];
-
-        scope.$on("colorpicker-selected", function($event, color){
-          scope.execCommand('foreColor', color.value);
-        });
-      }
+      scope.$on("colorpicker-selected", function ($event, color) {
+        scope.execCommand('foreColor', color.value);
+      });
     }
-);
+  }
+});
 
