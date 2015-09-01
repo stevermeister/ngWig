ac275e8aa69bce60db644ee205d1f3bbd41fbca1
bolelamx <bolelamx@gmail.com>
2015-09-02 00:11:21 +0300

    Added possibility to configure toolbar buttons and formats

--- src/javascript/app/ng-wig/ngWigToolbarProvider.js
+++ src/javascript/app/ng-wig/ngWigToolbarProvider.js
@@ -1,29 +1,61 @@
-angular.module('ngWig').provider('ngWigToolbarProvider', function () {
+angular.module('ngWig').provider('ngWigToolbar', function () {
+    var FORMAT_TYPE = 'format',
+        BUTTON_TYPE = 'button',
+        OTHER_TYPE = 'other';
 
-  var buttonLibrary = {
-    list1: {title: 'Unordered List', command: 'insertunorderedlist', styleClass: 'nw-button--unordered-list'},
-    list2: {title: 'Ordered List', command: 'insertorderedlist', styleClass: 'nw-button--ordered-list'},
-    bold: {title: 'Bold', command: 'bold', styleClass: 'nw-button--bold'},
-    italic: {title: 'Italic', command: 'italic', styleClass: 'nw-button--italic'},
-    link: {title: 'Link', command: 'createlink', styleClass: 'nw-button--link'}
-  };
-
-  var defaultButtonsList = ['list1', 'list2', 'bold', 'italic', 'link'];
-
-  this.$get = function () {
-    return {
-      getToolbarButtons: function(list) {
-        var toolbarButtons = [];
-        (list || defaultButtonsList).forEach(function(buttonKey) {
-          if(!buttonLibrary[buttonKey]) {
-            throw 'There is no "' + buttonKey + '" in your library. Possible variants: ' + Object.keys(buttonLibrary);
-          }
-          toolbarButtons.push(buttonLibrary[buttonKey]);
-        });
-        return toolbarButtons;
+    var itemLibrary = {
+      button: {
+          list1: {title: 'Unordered List', command: 'insertunorderedlist', styleClass: 'nw-button--unordered-list'},
+          list2: {title: 'Ordered List', command: 'insertorderedlist', styleClass: 'nw-button--ordered-list'},
+          bold: {title: 'Bold', command: 'bold', styleClass: 'nw-button--bold'},
+          italic: {title: 'Italic', command: 'italic', styleClass: 'nw-button--italic'},
+          link: {title: 'Link', command: 'createlink', styleClass: 'nw-button--link'}
+      },
+      format: {
+          p: {name: 'Normal text', value: 'p'},
+          h1: {name: 'Header 1', value: 'h1'},
+          h2: {name: 'Header 2', value: 'h2'},
+          h3: {name: 'Header 3', value: 'h3'}
       }
     };
-  };
+
+    var defaultConfig = {
+        button: ['list1', 'list2', 'bold', 'italic', 'link'],
+        format: ['p', 'h1', 'h2', 'h3'],
+        other: ['source']
+    };
+
+    function getToolbarItems(type, list) {
+      var items = [],
+          library = itemLibrary[type],
+          defaultList = defaultConfig[type] || [];
+
+      (list || defaultList).forEach(function (key) {
+          if (!library[key]) {
+              throw 'There is no "' + key + '" in your library. Possible variants: ' + Object.keys(library);
+          }
+          items.push(library[key]);
+      });
+      return items;
+    }
+
+    this.setConfig = function(config) {
+      defaultConfig  = config || {};
+    };
+
+    this.$get = function () {
+      return {
+          getToolbarButtons: function(list) {
+              return getToolbarItems(BUTTON_TYPE, list);
+          },
+          getToolbarFormats: function() {
+              return getToolbarItems(FORMAT_TYPE);
+          },
+          isSourceEnabled: function() {
+              return defaultConfig[OTHER_TYPE] && !!~defaultConfig[OTHER_TYPE].indexOf('source');
+          }
+      };
+    };
 
 
 });
