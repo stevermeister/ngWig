04105a1dd6c6e35e18ea27808ed8c174620154ad
bolelamx <bolelamx@gmail.com>
2015-09-16 12:40:08 +0300

    Added possibility to add custom simple buttons

--- src/javascript/app/ng-wig/ngWigToolbarProvider.js
+++ src/javascript/app/ng-wig/ngWigToolbarProvider.js
@@ -1,22 +1,38 @@
 angular.module('ngWig').provider('ngWigToolbar', function () {
 
   var buttonLibrary = {
-    list1: {title: 'Unordered List', command: 'insertunorderedlist', styleClass: 'nw-button--unordered-list'},
-    list2: {title: 'Ordered List', command: 'insertorderedlist', styleClass: 'nw-button--ordered-list'},
-    bold: {title: 'Bold', command: 'bold', styleClass: 'nw-button--bold'},
-    italic: {title: 'Italic', command: 'italic', styleClass: 'nw-button--italic'},
-    link: {title: 'Link', command: 'createlink', styleClass: 'nw-button--link'}
+    list1: {title: 'Unordered List', command: 'insertunorderedlist', styleClass: 'nw-button--unordered-list', isSimple: true},
+    list2: {title: 'Ordered List', command: 'insertorderedlist', styleClass: 'nw-button--ordered-list', isSimple: true},
+    bold: {title: 'Bold', command: 'bold', styleClass: 'nw-button--bold', isSimple: true},
+    italic: {title: 'Italic', command: 'italic', styleClass: 'nw-button--italic', isSimple: true},
+    link: {title: 'Link', command: 'createlink', styleClass: 'nw-button--link', isSimple: true}
   };
 
   var defaultButtonsList = ['list1', 'list2', 'bold', 'italic', 'link'];
 
   this.setButtons = function(buttons) {
-      if(!Array.isArray(buttons)) {
-          throw 'Argument "buttons" should be an array';
-      }
+    if(!Array.isArray(buttons)) {
+      throw 'Argument "buttons" should be an array';
+    }
 
-      defaultButtonsList = buttons;
+    defaultButtonsList = buttons;
   }
+
+  this.addSimpleButton = function (name, options) {
+    if (typeof name !== "string") {
+      throw 'Argument "name" should be a string';
+    }
+
+    var lowerCaseName = name.toLowerCase(),
+        defaultOptions = {
+          title: lowerCaseName[0].toUpperCase() + lowerCaseName.slice(1),
+          command: lowerCaseName,
+          styleClass: 'nw-button--' + lowerCaseName
+        };
+
+    buttonLibrary[name] = angular.extend({},defaultOptions, options, {isSimple: true});
+    defaultButtonsList.push(name);
+  };
 
   this.$get = function () {
     return {
