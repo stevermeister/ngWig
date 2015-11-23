6ecf0434878c48cd6ff5c4eccf9e88ea7525fe91
Stepan Suvorov <stevermeister@gmail.com>
2015-11-23 12:52:38 +0100

    update demo

--- demo/ng-wig.js
+++ demo/ng-wig.js
@@ -1,5 +1,5 @@
 /**
- * version: 2.3.1
+ * version: 2.3.2
  */
 angular.module('ngWig', ['ngwig-app-templates']);
 
@@ -210,6 +210,29 @@ angular.module('ngWig').provider('ngWigToolbar', function () {
 
 
 });
+angular.module('ngWig')
+    .config(['ngWigToolbarProvider', function (ngWigToolbarProvider) {
+       ngWigToolbarProvider.addCustomButton('formats', 'nw-formats-button');
+    }])
+    .directive('nwFormatsButton', function() {
+        return {
+            restrict: 'E',
+            replace: true,
+            template: '<select class="nw-select" ng-model="format" ng-change="execCommand(\'formatblock\', format.value)" ng-options="format.name for format in formats" ng-disabled="editMode"></select>',
+            link: function (scope) {
+                scope.formats = [
+                    {name: 'Normal text', value: 'p'},
+                    {name: 'Header 1', value: 'h1'},
+                    {name: 'Header 2', value: 'h2'},
+                    {name: 'Header 3', value: 'h3'}
+                ];
+
+                scope.format = scope.formats[0];
+            }
+        };
+    });
+
+
 angular.module('ngwig-app-templates', ['ng-wig/views/ng-wig.html']);
 
 angular.module("ng-wig/views/ng-wig.html", []).run(["$templateCache", function($templateCache) {
