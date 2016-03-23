c8a28290c76b5fc0b2155c41479e4f31a791eb97
Stepan Suvorov <stevermeister@gmail.com>
2016-03-23 11:42:41 +0100

    plugins components

--- src/javascript/app/plugins/forecolor.ngWig.js
+++ src/javascript/app/plugins/forecolor.ngWig.js
@@ -1,17 +1,13 @@
 angular.module('ngWig')
-    .config(['ngWigToolbarProvider', function (ngWigToolbarProvider) {
-       ngWigToolbarProvider.addCustomButton('forecolor', 'nw-forecolor-button');
-    }])
-    .directive('nwForecolorButton', function() {
-        return {
-            restrict: 'E',
-            replace: true,
-            template: '<button colorpicker ng-model="fontcolor" ng-disabled="editMode" colorpicker-position="right" class="nw-button font-color" title="Font Color" ng-disabled="isDisabled">Font Color</button>',
-            link: function (scope) {
-                scope.$on('colorpicker-selected', function ($event, color) {
-                    scope.execCommand('foreColor', color.value);
-                });
-            }
-        };
-    });
+  .config(['ngWigToolbarProvider', function(ngWigToolbarProvider) {
+    ngWigToolbarProvider.addCustomButton('forecolor', 'nw-forecolor-button');
+  }])
+  .component('nwForecolorButton', {
+    template: '<button colorpicker ng-model="fontcolor" ng-disabled="editMode" colorpicker-position="right" class="nw-button font-color" title="Font Color" ng-disabled="isDisabled">Font Color</button>',
+    controller: function($scope) {
+      $scope.$on('colorpicker-selected', function($event, color) {
+        $scope.execCommand('foreColor', color.value);
+      });
+    }
+  });
 
