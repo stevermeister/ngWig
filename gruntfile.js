2e61587ce0743447949e7c7808579cf42454e5a1
Aristeidis Bampakos <bampakoa@gmail.com>
2016-07-21 18:36:18 +0300

    Added single run test mode.

--- gruntfile.js
+++ gruntfile.js
@@ -92,6 +92,11 @@ module.exports = function(grunt) {
     },
     karma: {
       unit: {
+        configFile: 'karma.conf.js',
+        autoWatch: false,
+        singleRun: true
+      },
+      continuous: {
         configFile: 'karma.conf.js'
       }
     }
--- karma.conf.js
+++ karma.conf.js
@@ -17,10 +17,6 @@ module.exports = function (config) {
             './src/javascript/app/**/!(ng-wig|app).js'
         ],
         
-        proxies: {
-            '/': 'http://localhost:8888/'
-        },
-
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
@@ -47,17 +43,10 @@ module.exports = function (config) {
         // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
         logLevel: config.LOG_INFO,
         
-        // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: true,
-        
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         //        browsers: ['Chrome', 'ChromeCanary', 'FirefoxAurora', 'Safari', 'PhantomJS'],
-        browsers: ['Chrome'],
-        
-        // Continuous Integration mode
-        // if true, Karma captures browsers, runs the tests and exits
-        singleRun: false
+        browsers: ['Chrome']
     });
 };
 
