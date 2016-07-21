c2c3ba8f32c4f717ba4feddd405759a9d77c9280
Aristeidis Bampakos <bampakoa@gmail.com>
2016-07-21 20:10:43 +0300

    Added component level tests for the main component.

---
+++ src/javascript/app/ng-wig/ng-wig.component.spec.js
@@ -0,0 +1,172 @@
+describe('component: ngWig', () => {
+    var component;
+    var scope;
+    var content;
+    var options;
+    var buttons;
+    var beforeExecCommand;
+    var afterExecCommand;
+    var element;
+    var attrs;
+    var pasteSpy;
+    var beforeExecSpy;
+    var afterExecSpy;
+    var mocks;
+    var window;
+
+    beforeEach(module('ngWig'));
+
+    beforeEach(inject((_$componentController_, _$rootScope_, _$window_, _ngWigToolbar_) => {
+        window = _$window_;
+
+        scope = _$rootScope_.$new();
+
+        content = 'Fake text';
+        element = angular.element('<div></div>');
+        attrs = { $observe: new Function() };
+        options = ['Option 1', 'Option 2'];
+        buttons = 'button1,button2';
+        pasteSpy = jasmine.createSpy('pasteSpy');
+        beforeExecSpy = jasmine.createSpy('beforeExecSpy');
+        afterExecSpy = jasmine.createSpy('afterExecSpy');
+
+        mocks = {
+            list1: {title: 'Unordered List', command: 'insertunorderedlist', styleClass: 'list-ul'},
+            list2: {title: 'Ordered List', command: 'insertorderedlist', styleClass: 'list-ol'},
+            bold: {title: 'Bold', command: 'bold', styleClass: 'bold'},
+            italic: {title: 'Italic', command: 'italic', styleClass: 'italic'},
+            link: {title: 'Link', command: 'createlink', styleClass: 'link'}
+        };
+
+        spyOn(_ngWigToolbar_, 'getToolbarButtons').and.returnValue(mocks);
+        spyOn(window.getSelection(), 'removeAllRanges');
+        spyOn(window, 'prompt');
+        spyOn(scope, '$broadcast');
+
+        component = _$componentController_('ngWig', 
+                                            { $scope: scope, $element: element, $attrs: attrs }, 
+                                            { content: content, options: options, buttons: buttons, onPaste: pasteSpy, beforeExecCommand: beforeExecSpy, afterExecCommand: afterExecSpy }
+                                            );
+    }));
+
+    it('should expose content', () => {
+        expect(component.content).toEqual(content);
+    });
+
+    it('should expose options', () => {
+        expect(component.options).toEqual(options);
+    });
+
+    it('should expose buttons', () => {
+        expect(component.buttons).toEqual(buttons);
+    });
+
+    it('should call onPaste', function(){
+        component.onPaste();
+
+        expect(pasteSpy).toHaveBeenCalled();
+    });
+
+    it('should call beforeExecCommand', function(){
+        component.beforeExecCommand();
+
+        expect(beforeExecSpy).toHaveBeenCalled();
+    });
+
+    it('should call afterExecCommand', function(){
+        component.afterExecCommand();
+
+        expect(afterExecSpy).toHaveBeenCalled();
+    });
+
+    it('should set required', () => {
+        expect(component.required).toEqual(false);
+    });
+
+    it('should set isSourceModeAllowed', () => {
+        expect(component.isSourceModeAllowed).toEqual(false);
+    });
+
+    it('should set editMode', () => {
+        expect(component.editMode).toEqual(false);
+    });
+
+    it('should set toolbarButtons', () => {
+        expect(component.toolbarButtons).toBe(mocks);
+    });
+
+    describe('isEditorActive function', () => {
+        it('should exist', () => {
+            expect(component.isEditorActive).not.toBe(null);
+        });
+
+        it('should return false', () => {
+            expect(component.isEditorActive()).toEqual(false);
+        });
+    });
+
+    describe('toggleEditMode function', () => {
+        it('should exist', () => {
+            expect(component.toggleEditMode).not.toBe(null);
+        });
+
+        it('should toggle the edit mode of the editor', () => {
+            component.toggleEditMode();
+
+            expect(component.editMode).toEqual(true);
+        });
+
+        it('should remove all ranges from the selection', () => {
+            component.toggleEditMode();
+
+            expect(window.getSelection().removeAllRanges).toHaveBeenCalled();
+        });
+    });
+
+    describe('execCommand function', () => {
+        it('should exist', () => {
+            expect(component.execCommand).not.toBe(null);
+        });
+
+        it('should return if the editor is in edit mode', () => {
+            component.editMode = true;
+
+            expect(component.execCommand('fakeCmd')).toEqual(false);
+        });
+
+        it('should broadcast the command', () => {
+            component.execCommand('fakeCmd', {});
+
+            expect(beforeExecSpy).toHaveBeenCalledWith({
+                command: 'fakeCmd',
+                options: {}
+            });
+            expect(scope.$broadcast).toHaveBeenCalledWith('execCommand', {
+                command: 'fakeCmd',
+                options: {}
+            });
+            expect(afterExecSpy).toHaveBeenCalledWith({
+                command: 'fakeCmd',
+                options: {}
+            });
+        });
+
+        it('should show a prompt when the command name is createlink', () => {
+            component.execCommand('createlink');
+
+            expect(window.prompt).toHaveBeenCalledWith('Please enter the URL', 'http://');
+        });
+
+        it('should show a prompt when the command name is insertImage', () => {
+            component.execCommand('insertImage');
+
+            expect(window.prompt).toHaveBeenCalledWith('Please enter the URL', 'http://');
+        });
+
+        it('should not show a prompt', () => {
+            component.execCommand('fakeCmd');
+
+            expect(window.prompt).not.toHaveBeenCalled();
+        });
+    });
+});
