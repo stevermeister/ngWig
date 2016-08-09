2d0930ca76e5b0a7bd8736ad5350cb02c219de6a
Stepan Suvorov <stevermeister@gmail.com>
2016-08-09 09:42:00 +0200

    Release v3.0.7

--- dist/ng-wig.js
+++ dist/ng-wig.js
@@ -1,11 +1,12 @@
 'use strict';
 
 /**
- * version: 3.0.6
+ * version: 3.0.7
  */
+var VERSION = '3.0.6';
 angular.module('ngWig', ['ngwig-app-templates']);
 angular.ngWig = {
-  version: '3.0.4'
+  version: VERSION
 };
 
 angular.module('ngWig').component('ngWig', {
--- src/javascript/app/ng-wig/ng-wig.js
+++ src/javascript/app/ng-wig/ng-wig.js
@@ -1,7 +1,8 @@
 /**
- * version: 3.0.6
+ * version: 3.0.7
  */
+var VERSION = '3.0.6';
 angular.module('ngWig', ['ngwig-app-templates']);
 angular.ngWig = {
-  version: '3.0.4'
+  version: VERSION
 };
